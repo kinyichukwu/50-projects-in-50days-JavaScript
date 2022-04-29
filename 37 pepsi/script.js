ebbc75ca06bc408a115dceac2357ae8414c8c7fc
Kinyichukwu Oselukwue <81447346+kinyichukwu@users.noreply.github.com>
2022-04-29 17:12:10 +0100

    pepsi website design

---
+++ 37 pepsi/script.js
@@ -0,0 +1,13 @@
+let pepsi = document.querySelector('.pepsi')
+let home = document.querySelector('.sec')
+const body = document.querySelector('.section')
+
+
+function imgS(anything){
+    pepsi.src = anything;
+}
+
+
+function background(x){
+    body.style.backgroundColor = x;
+}
